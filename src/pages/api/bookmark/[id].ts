1727603c21d52fece96753a7ff98a702f5ba650f
kou12345 <hi.0412.ko.2002@gmail.com>
2023-04-29 17:02:45 +0900

    sqliteからPlanetScaleに変更

--- src/pages/api/bookmark/[id].ts
+++ src/pages/api/bookmark/[id].ts
@@ -1,54 +1,57 @@
-import db from "@/utils/sqlite";
+import connectToDatabase from "@/utils/mysql";
+import type { RowDataPacket } from "mysql2";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // curl http://localhost:3000/api/bookmark/1
+  if (req.method === "GET") return await getOneHandler(req, res);
 
-  if (method === "GET") return getOneHander(req, res);
-  if (method === "PUT") return putHandler(req, res);
-  if (method === "DELETE") return deleteHandler(req, res);
+  // curl -X PUT -H "Content-Type: application/json" -d '{"title":"example", "url":"https://example.com"}' http://localhost:3000/api/bookmark/1
+  if (req.method === "PUT") return await putHandler(req, res);
+
+  // curl -X DELETE http://localhost:3000/api/bookmark/1
+  if (req.method === "DELETE") return await deleteHandler(req, res);
+
+  // 未対応のHTTPメソッドの場合
+  res.status(405).json({ error: "Method not allowed" });
 }
 
-const getOneHander = (req: NextApiRequest, res: NextApiResponse) => {
+const getOneHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
+  const connection = await connectToDatabase();
 
-  db.all("SELECT * FROM bookmarks WHERE id = ?", [id], (err, rows) => {
-    if (err) {
-      console.error(err.message);
-      return res.status(500).json({ error: "Database query error" });
-    }
-
-    return res.status(200).json(rows[0]);
-  });
+  try {
+    const [rows] = await connection.query<RowDataPacket[]>("SELECT * FROM bookmarks WHERE id = ?", [id]);
+    res.status(200).json(rows[0]);
+  } catch (err: unknown) {
+    console.error((err as Error).message);
+    res.status(500).json({ error: "Database query error" });
+  }
 };
 
-const putHandler = (req: NextApiRequest, res: NextApiResponse) => {
+const putHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
   const { title, url } = req.body;
+  const db = await connectToDatabase();
 
-  db.run(
-    "UPDATE bookmarks SET title = ?, url = ? WHERE id = ?",
-    [title, url, id],
-    function (err) {
-      if (err) {
-        console.error(err.message);
-        return res.status(500).json({ error: "Database query error" });
-      }
-
-      return res.status(200).json({ id: id });
-    }
-  );
+  try {
+    await db.query("UPDATE bookmarks SET title = ?, url = ? WHERE id = ?", [title, url, id]);
+    res.status(200).json({ id: id });
+  } catch (err: unknown) {
+    console.error((err as Error).message);
+    res.status(500).json({ error: "Database query error" });
+  }
 };
 
-const deleteHandler = (req: NextApiRequest, res: NextApiResponse) => {
+const deleteHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
+  const db = await connectToDatabase();
 
-  db.run("DELETE FROM bookmarks WHERE id = ?", [id], function (err) {
-    if (err) {
-      console.error(err.message);
-      return res.status(500).json({ error: "Database query error" });
-    }
-
-    return res.status(200).json({ id: id });
-  });
+  try {
+    await db.query("DELETE FROM bookmarks WHERE id = ?", [id]);
+    res.status(200).json({ id: id });
+  } catch (err: unknown) {
+    console.error((err as Error).message);
+    res.status(500).json({ error: "Database query error" });
+  }
 };
